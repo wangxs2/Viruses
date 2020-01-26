4f8a6a85107cb31d33a6f0d32a799fb86ada7c8a
wangxs <15037589151@163.com>
2020-01-26 18:48:35 +0800

    12

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Mint from 'mint-ui';
+Vue.use(Mint);
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,12 +5,7 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
 }
 </script>

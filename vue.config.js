d1a20b004261d0215c9fb016a748372940fc1073
wangxs <15037589151@163.com>
2020-01-26 17:51:41 +0800

    测试

---
+++ vue.config.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack')
+// const path = require('path')
+
+// function resolve(dir) {
+//   return path.join(__dirname, '..'1, dir)
+// }
+
+module.exports = {
+  lintOnSave: false,
+  publicPath: './', // 部署项目路径
+  // baseUrl: process.env_NODE_ENV == 'production' ? '/vehiclerepair/' : '', // 部署项目路径
+  devServer: {
+    port: 8086, // 端口号
+    // host: 'localhost',
+    open: true, //配置自动启动浏览器
+    proxy: {
+      '/vehiclerepair': {
+        target: 'http://114.80.231.180:8088//vehiclerepair',
+        // target: 'http://10.1.30.208:50067/vehiclerepair',
+        // target: 'http://192.168.1.115:8080/vehiclerepair',
+        // target: 'http://10.1.30.207:50045/vehiclerepair',
+        // target: 'http://10.1.30.178:8080/vehiclerepair',
+        // target: 'http://10.1.4.106:8080/vehiclerepair',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/vehiclerepair': '/'
+        }
+      }
+    }
+  },
+
+  configureWebpack: {
+    // webpack 配置项
+    // resolve: {
+    //   alias: {
+    //     vue$: 'vue/dist/vue.esm.js'
+    //     // '@': resolve('src'),
+    //     // 'views': resolve('src/views')
+    //   }
+    // },
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'windows.jQuery': 'jquery'
+      })
+    ]
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        // data: `@import "@/assets/css/global.scss";`
+      }
+    }
+  }
+}

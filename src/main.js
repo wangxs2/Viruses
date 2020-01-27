b4e9902d9d1b1b6c1c0c296d2154ac0d07ef9374
wangxs <15037589151@163.com>
2020-01-27 11:07:54 +0800

    12

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Mint from 'mint-ui';
 import 'amfe-flexible'
-Vue.use(Mint);
+import Vant from 'vant';
+import common from './libs/common.js'
+import 'vant/lib/index.css';
+Vue.use(Vant);
+Vue.use(common);
 Vue.config.productionTip = false
 new Vue({
   router,
